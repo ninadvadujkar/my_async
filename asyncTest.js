3fee0c9cdb611e9ca3babf075dbf1191b9abe4d7
Ninad.Vadujkar <ninad.vadujkar@ril.com>
2017-09-14 11:50:06 +0530

    Added jsdoc. Updated waterfall method -> removed unnecessary if-else

--- asyncTest.js
+++ asyncTest.js
@@ -5,63 +5,61 @@ var funcs = [
     (cb) => {
         setTimeout(() => {
             cb(null, 'one');
-        }, 5000)
+        }, 5000);
     },
     (cb) => {
         setTimeout(() => {
             cb(null, 'two');
-        }, 3000)
+        }, 3000);
     },
     (cb) => {
         setTimeout(() => {
             cb(null, 'three');
-        }, 1000)
+        }, 1000);
     },
     (cb) => {
         setTimeout(() => {
             cb(null, 'four');
-        }, 2000)
+        }, 2000);
     }
 ];
-
-async.waterfall([
-    (cb) => {
-        return cb(null, 'one', 'two');
-    },
-    (arg1, arg2, cb) => {
-        return cb(null, 'final');
-    }
-
-], (err, resp) => {
-    console.log("Done!");
+console.log("Testing parallelLimit execution!!!");
+async.parallelLimit(funcs, 2, function(err, resp) {
     console.log(err, resp);
-});
-let arr = [1, 2, 3, 4, 5];
-/*console.log("Testing parallel execution!!!");
-async.parallel(funcs, function(err, res) {
-    if(err) throw err;
-    console.log(res);
-    console.log("Testing series execution!!!");
-    async.series(funcs, function(err, res) {
-        if(err) throw err;
+    let arr = [1, 2, 3, 4, 5];
+    console.log("Testing parallel execution!!!");
+    async.parallel(funcs, function(err, res) {
         console.log(res);
-        console.log("Testing parallel limit execution!!!");
-        async.parallelLimit(funcs, 2, function(err, resp) {
-            if(err) throw err;
-            console.log(resp);
+        console.log("Testing series execution!!!");
+        async.series(funcs, function(err, res) {
+            console.log(res);
+            console.log("Testing parallel limit execution!!!");
+            async.parallelLimit(funcs, 2, function(err, resp) {
+                console.log(resp);
+                console.log("Testing each execution!!!");
+                async.each(arr, (i, callback) => {
+                    setTimeout(() => {
+                        console.log("Value of i " + i);
+                        //if(i == 2) return callback(true);
+                        return callback();
+                    }, 1000);
+                }, (err) => {
+                    console.log(err);
+                    console.log("Testing waterfall execution!!!");
+                    async.waterfall([
+                        (cb) => {
+                            return cb(null, 'one', 'two');
+                        },
+                        (arg1, arg2, cb) => {
+                            return cb(null, 'final');
+                        }
+
+                    ], (err, resp) => {
+                        console.log(err, resp);
+                        console.log("Done!");
+                    });
+                });       
+            });
         });
     });
-});*/
-
-/*async.each(arr, (i, callback) => {
-    setTimeout(() => {
-        console.log("Value of i " + i);
-        //if(i == 2) return callback(true);
-        return callback();
-    }, 1000);
-}, (err) => {
-    if(err) {
-        throw err;
-    }
-    console.log("Done!!!");
-});*/
+});
